5a8dc7d54e42e06b2b3f67c677c2bfd57d360106
Jim McMahon <mcmahonj@mail.nih.gov>
2009-12-10 17:53:23 +0000

    removed code that should not have been in trunk.  Jim McMahon
    SVN-Revision: 5

--- compile.bat
+++
@@ -1,7 +0,0 @@
-CD c:\
-
-cd C:\dev\caIMAGE\webapps\WEB-INF\classes\gov\nih\nci\caIMAGE
-
-javac -classpath C:\Tomcat4.1\common\lib\servlet.jar;C:\dev\caimage\webapps\WEB-INF\lib\cabio.jar;C:\dev\caimage\webapps\WEB-INF\lib\mmhccdb.jar;C:\dev\caimage\webapps\WEB-INF\lib\caimage.jar;C:\dev\caimage\webapps\WEB-INF\lib\multipartformdata-1.3.jar;C:\dev\caimage\webapps\WEB-INF\lib\NetComponents.jar;C:\dev\caimage\webapps\WEB-INF\lib\activation.jar;C:\dev\caimage\webapps\WEB-INF\lib\mail.jar;C:\dev\caimage\webapps\WEB-INF\lib\OntylogDTS.jar;C:\dev\caimage\webapps\WEB-INF\lib\ApelonCommon.jar;C:\dev\caimage\webapps\WEB-INF\lib\jaxp.jar;C:\dev\caimage\webapps\WEB-INF\lib\classes12_01.jar;C:\dev\caimage\webapps\WEB-INF\lib\log4j.jar;C:\dev\caimage\webapps\WEB-INF\lib\parser.jar; *.java ui\bean\*.java util\*.java Servlet\*.java 
-
-CD c:\dev\caimage
